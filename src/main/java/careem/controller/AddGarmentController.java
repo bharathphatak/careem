5a61307ce2b7642c390e999c68a29a10f0fb5ed6
bharath <bharath.phatak@gmail.com>
2017-02-25 21:11:27 +0530

    Added JDBC connection

--- src/main/java/careem/controller/AddGarmentController.java
+++ src/main/java/careem/controller/AddGarmentController.java
@@ -1,5 +1,7 @@
 package careem.controller;
 
+import careem.DataLayer.DatabaseConnection;
+import careem.DataLayer.Partner;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,10 +25,37 @@ public class AddGarmentController {
         return "Hello";
     }
 
-    @RequestMapping(value = "/api/add/get/url", method = RequestMethod.POST)
+    //The values will be seperated by $
+    @RequestMapping(value = "/api/add/partner", method = RequestMethod.POST)
     @ResponseBody
-    public String teat(@RequestBody String value){
-        return "Hello";
+    public void addPartner(@RequestBody String partner){
+        Partner partnerObject = getPartner(partner);
+        DatabaseConnection databaseConnection = new DatabaseConnection();
+        databaseConnection.addPartner(partnerObject);
+
+    }
+
+    @RequestMapping(value = "/api/remove/{name}/partner", method = RequestMethod.DELETE)
+    @ResponseBody
+    public void removePartner(@PathVariable String partnerName){
+        DatabaseConnection databaseConnection = new DatabaseConnection();
+        databaseConnection.removePartner(partnerName);
+    }
+
+
+
+    public Partner getPartner(String partner){
+        String[] values = partner.split("$");
+
+        String name = values[0];
+        String transportType = values[1];
+        int quantity = Integer.parseInt(values[2]);
+        int cost = Integer.parseInt(values[3]);
+        String outCitySupport = values[4];
+        String location = values[5];
+        Partner partnerObject = new Partner(name,transportType,quantity,cost,outCitySupport,location);
+
+        return partnerObject;
     }
 
 
